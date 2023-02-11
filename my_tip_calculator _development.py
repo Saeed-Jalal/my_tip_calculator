52180029321c16cb62243dab770c414555a72618
Saeed jalal <syedsaeedjalal@yahoo.com>
2023-02-11 12:38:57 +0200

    my_first_tip_calculator

--- my_tip_calculator _development.py
+++ my_tip_calculator _development.py
@@ -8,10 +8,10 @@ print("Welcome to my Tip calculator!")
 I am using var to store the output and 
 input function to take and also using float function
 '''
-bill = float(input("What was the total bill without tip? $"))
+bill_without_tip = float(input("What was the total bill without tip? $"))
 tip = int(input("How much you would like to tip? 5, 10, 15 :"))
 group_of_people = int(input("How many people would like to split the bill?"))
-total_bill = bill + tip
+total_bill = bill_without_tip + tip
 bill_per_person = total_bill / group_of_people
 #Using round function to round the bill in two decimal
 final_amount = round(bill_per_person, 2)
